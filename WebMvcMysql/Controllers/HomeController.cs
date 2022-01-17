65082724565d6368dbae2dac7597ace50f40b869
GabrielBassalo <gabrielbassalo@outlook.com>
2022-01-17 18:38:30 -0300

    Alterações

--- WebMvcMysql/Controllers/HomeController.cs
+++ WebMvcMysql/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace WebMvcMysql.Controllers
         {
             return View();
         }
+        public IActionResult Carousel()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
